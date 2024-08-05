2cfcc99ce531e08865d2a3c4f340f79783bc4097
Ava <azare3@asu.edu>
2024-08-05 13:28:29 -0700

    updated file

--- Notebook/structure_editting_scripts/ER_functions.py
+++ Notebook/structure_editting_scripts/ER_functions.py
@@ -1,16 +1,20 @@
 import os
 import numpy as np
+from itertools import combinations
 import random
+import logging
+from typing import Dict, Tuple, List
 from Metabackbone_functions import (load_dna_structure_files, find_longest_strand, find_cross_over_in_longest_strand, calculate_left_right_pos, find_valid_point, find_bases_around_point, calculate_center_of_mass, calculate_bend_angle, find_bend_angle, find_bases_in_sphere, remove_three_strands_in_sphere, export_dna_structures, run_all_simulations)
 from ipy_oxdna.dna_structure import DNAStructure, DNAStructureStrand, load_dna_structure, DNABase, strand_from_info
 from ipy_oxdna.oxdna_simulation import Simulation, SimulationManager
 
-    
-  
+
+
 def print_colored(message, color_code):
     print(f"\033[{color_code}m{message}\033[0m")
-
-# ANSI color codes
+    
+    
+# ANSI color codes for printing colored messages
 colors = {
     'blue': '34',
     'green': '32',
@@ -33,7 +37,32 @@ def load_simulated_structure(structure_id, sim_base_path):
     dna = load_dna_structure(top_path, dat_path)
     return dna
 
-# Check the structure of the DNA object
+
+
+def create_index_position_map(dna) -> Dict[Tuple[float, float, float], int]:
+    index_position_map = {}
+    for strand in dna.strands:
+        for base in strand:
+            index_position_map[tuple(base.pos)] = base.uid
+    return index_position_map
+
+
+
+def get_indexes_from_positions(new_positions: List[np.ndarray], index_position_map: Dict[Tuple[float, float, float], int]) -> Dict[Tuple[float, float, float], int]:
+    new_index_map = {}
+    for pos in new_positions:
+        key = tuple(pos)
+        if key in index_position_map:
+            new_index_map[key] = index_position_map[key]
+        else:
+            # Handle the case where the position is not found
+            logging.warning(f"Position {key} not found in the initial map. It may indicate that the base has moved significantly or is new.")
+            raise ValueError(f"Position {key} not found in the initial map.")
+    return new_index_map
+
+
+
+
 def check_dna_structure(dna):
     print_colored(f"DNA type: {type(dna)}", colors['blue'])
     if hasattr(dna, 'strands'):
@@ -47,6 +76,8 @@ def check_dna_structure(dna):
     else:
         print_colored("DNA does not have 'strands' attribute.", colors['red'])
 
+
+
 def evaluate_fitness(angles, desired_angle, tolerance):
     fitness_scores = []
     for angle in angles:
@@ -55,8 +86,8 @@ def evaluate_fitness(angles, desired_angle, tolerance):
         fitness_scores.append(fitness_score)
     return fitness_scores
 
+
 def run_simulations_for_structure(structure_id, base_path, sim_base_path, rel_parameters, eq_parameters, prod_parameters):
-    print_colored(f"Starting simulations for structure {structure_id}...", colors['yellow'])
     file_dir = os.path.join(base_path, f'structure_{structure_id}')
     sim_path = os.path.join(sim_base_path, f'2268_bp_{structure_id}')
     
